360c78bf95c2f82e99573c61f6f36b976f361869
utyujinn <hg33fah9@utyujin.com>
2023-11-04 13:15:23 +0900

    add mount

--- 1.sh
+++ 1.sh
@@ -44,6 +44,10 @@ ExecStart=
 ExecStart=-/usr/bin/agetty --autologin utyujin --noclear %I $TERM
 " > /etc/systemd/system/getty@tty1.service.d
 
+
+#mount
+echo "/dev/nvme0n1p2 /run/media/utyujin/data ntfs-3g uid=utyujin,gid=wheel,umask=0022 0 2" | sudo tee -a /mnt/data
+
 #preparation
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2.sh > /home/utyujin/2.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/3.sh > /home/utyujin/3.sh
