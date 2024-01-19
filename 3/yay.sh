135d3381c00438a4cc393cb73a16f1b6b9f6f335
utyujin <119794701+utyujinn@users.noreply.github.com>
2024-01-19 23:01:30 +0900

    Update yay.sh

--- 3/yay.sh
+++ 3/yay.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sudo pacman -S make fakeroot gcc
 # yay
 sudo pacman -S git fakeroot binutils make gcc
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+sudo pacman -S pkg-config flex bison
+yay -S autofs
