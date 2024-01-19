c5a4bf9237e9a790b252b05ad0ac34253e8d5d09
utyujin <119794701+utyujinn@users.noreply.github.com>
2024-01-19 21:45:16 +0900

    Update get.sh

--- 0/get.sh
+++ 0/get.sh
@@ -6,10 +6,10 @@ curl https://raw.githubusercontent.com/utyujinn/archinstall/master/0/0.sh > 0.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/kernel.sh > kernel.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/bootloader.sh > bootloader.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/1.sh > 1.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/chroot.sh > /chroot.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/chroot.sh > chroot.sh
 
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/2.sh > /mnt/2.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/base.sh > /mnt/base.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/desktop.sh > /mnt/chroot.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/settings.sh > /mnt/chroot.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/user.sh > /mnt/chroot.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/desktop.sh > /mnt/desktop.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/settings.sh > /mnt/settings.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/user.sh > /mnt/user.sh
