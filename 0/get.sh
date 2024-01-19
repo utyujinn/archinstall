ba445cdffd5ee3a7bd74d08be574a0083d58b3c9
utyujin <119794701+utyujinn@users.noreply.github.com>
2024-01-19 21:31:37 +0900

    Update get.sh

--- 0/get.sh
+++ 0/get.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/0/env.sh > env.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/0/mount.sh > env.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/0/mount.sh > mount.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/0/0.sh > 0.sh
 
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/kernel.sh > kernel.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/bootloader.sh > bootloader.sh
-curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1.sh > 1.sh
+curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/1.sh > 1.sh
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/1/chroot.sh > /chroot.sh
 
 curl https://raw.githubusercontent.com/utyujinn/archinstall/master/2/2.sh > /mnt/2.sh
